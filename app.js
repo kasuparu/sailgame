09e9de49254c9146edfaf4cad8fc76d1859ead4e
Vladimir Drynov <vladimir.drynov@nfq.lt>
2014-07-25 14:45:35 +0300

    Can run without an error

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var config = require('./config/config');
 global.config = config;
 
 var express = require('express');
+var winston = require('winston');
+var http = require('http');
+
+var loglevel = 'development' === process.env.env ? 'info' : (config.loglevel || 'info');
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({
+            filename: 'app.log',
+            level: loglevel
+        })
+    ]
+});
 
 var app = express();
 
@@ -15,11 +28,12 @@ app.use(express.logger('dev'));
 // development only
 if ('development' === app.get('env')) {
     app.use(express.errorHandler());
+	
 }
 
 var server = http.createServer(app);
 
-server.listen(app.get('port'), app.get('host'), function(){
+server.listen(app.get('port'), app.get('host'), function() {
     logger.info(
         'Express server listening on %s:%s, %s mode',
         app.get('host'),
