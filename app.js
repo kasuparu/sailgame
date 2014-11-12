2363bfebe551b0e68f411398f5ee8b92d9efebe8
kasuparu <kasuparu@about.me>
2014-11-12 13:14:45 +0200

    #broken RequireJS, WIP Phaser on Node

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 "use strict";
 var fs = require('fs');
 
-if (fs.existsSync('./config/config.js')) {
-	var config = require('./config/config');
-} else {
-	var config = require('./config/config.js.dist');
-};
+var config;
 
+if (fs.existsSync('./config/config.js')) {
+	config = require('./config/config');
+} else {
+	config = require('./config/config.js.dist');
+}
 
 global.config = config;
 
 var express = require('express');
 var winston = require('winston');
 var http = require('http');
+var path = require('path');
+var requireJs = require('requirejs');
+var requireJsConfig = require('./config/requirejs.json');
+
+requireJsConfig.baseUrl = path.join(__dirname, '/static/src');
+console.log(requireJsConfig.baseUrl);
+requireJsConfig.nodeRequire = require;
+
+for (var pathName in requireJsConfig.paths) {
+    if (requireJsConfig.paths.hasOwnProperty(pathName)) {
+        requireJsConfig.paths[pathName] = path.join(requireJsConfig.baseUrl, requireJsConfig.paths[pathName]);
+        console.log(requireJsConfig.paths[pathName]);
+    }
+}
+
+requireJs.config(requireJsConfig);
 
 var loglevel = 'development' === process.env.env ? 'info' : (config.loglevel || 'info');
 
@@ -121,4 +138,31 @@ io.sockets.on('connection', function (socket) {
 
 });
 
+requireJs([
+    'socket.io',
+    'Phaser',
+    'BasicGame',
+    'BasicGameBoot',
+    'BasicGamePreloader',
+    'BasicGameMainMenu',
+    'BasicGameGame'
+], function (io, Phaser, BasicGame) {
 
+    console.log('requireJs callback');
+
+    //	Create your Phaser game and inject it into the game div.
+    //	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
+    //	We're using a game size of 1024 x 768 here, but you can use whatever you feel makes sense for your game of course.
+    var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game');
+
+    //	Add the States your game has.
+    //	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+    game.state.add('Boot', BasicGame.Boot);
+    game.state.add('Preloader', BasicGame.Preloader);
+    game.state.add('MainMenu', BasicGame.MainMenu);
+    game.state.add('Game', BasicGame.Game);
+
+    //	Now start the Boot state.
+    game.state.start('Boot');
+
+});
