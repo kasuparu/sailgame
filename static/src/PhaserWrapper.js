6dba1cc9e527318e69f7ec1414ab2d878556a8a8
kasuparu <kasuparu@about.me>
2014-11-14 14:42:19 +0200

    #broken Phaser wrapper

---
+++ static/src/PhaserWrapper.js
@@ -0,0 +1,29 @@
+/*global global */
+/*global canvas */
+/*global jsdom */
+// https://gist.github.com/crisu83/5857c4a638e57308be4f#file-phaser-js
+// this is an ingenius hack that allows us to run Phaser without a browser
+// ... and yes, it took some time to figure out how to do this
+
+var jsdom = require('jsdom');
+var canvas = require('canvas');
+
+var document = jsdom.jsdom(null);
+var window = document.parentWindow;
+var Phaser;
+
+// expose a few things to all the modules
+global.document = document;
+global.window = window;
+global.canvas = canvas;
+global.Image = canvas.Image;
+global.window.CanvasRenderingContext2D = 'foo'; // let Phaser think that we have a canvas
+global.window.Element = undefined;
+global.navigator = {userAgent: 'Custom'}; // could be anything
+
+// fake the xml http request object because Phaser.Loader uses it
+global.XMLHttpRequest = function() {};
+
+// load an expose PIXI in order to finally load Phaser
+global.PIXI = require('../../phaser/build/custom/pixi');
+global.Phaser = Phaser = require('../../phaser/build/custom/phaser-arcade-physics');
